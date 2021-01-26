719dc21e3cd1741ae192b7310b32427ec5a8120d
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-26 18:40:08 -0500

    created the post route for the app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-        let hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    let hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
+router.post("/api/burgers", function (req, res) {
+  burger.create(["burger_name"], [req.body.name], function (result) {
+    res.json({ id: result.insertId });
+  });
+});
