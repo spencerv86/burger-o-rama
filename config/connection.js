15dc13f6a7bd463bbfa81fd6ade9131464f03477
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-25 22:56:39 -0500

    added jawsdb code to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "LS2jNHzXtr[LQB&+4I#y",
-  database: "cat_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "LS2jNHzXtr[LQB&+4I#y",
+    database: 'cat_db'
+  });
+};
+
 
 // Make connection.
 connection.connect(function(err) {
