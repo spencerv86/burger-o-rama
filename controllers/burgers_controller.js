048565a24bb563ce27397b2f375d9a17e4b8cbc1
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-26 18:42:44 -0500

    created the update/PUT route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,3 +17,20 @@ router.post("/api/burgers", function (req, res) {
     res.json({ id: result.insertId });
   });
 });
+
+router.put("api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+    burger.update(
+        {
+            devoured: req.body.devoured,
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        }
+    );
+});
