397982d0a7cd6d3f7ecb3ff8e8c31b9f7d107448
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-26 19:25:12 -0500

    created burgers.js with ajax functions to be accessed by the handlebars pages

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,33 @@
+$(function () {
+    $(".change-hunger").on("click", function(event) {
+        const id = $(this).data("id");
+        const hunger = $(this).data("hunger");
+
+        const hungerState = {
+            devoured: hunger,
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: hungerState,
+        }).then(function () {
+            location.reload();
+        });
+    });
+
+    $(#add-bgr).on("click", function(event) {
+        event.preventDefault();
+
+        const newBurger = {
+            name: $("#bgr").val().trim(),
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger,
+        }).then(function() {
+            console.log("Ordered new burger");
+            location.reload();
+        });
+    });
+});
