ac84b28c89b4d44c566d500501e4005609f13540
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-26 19:29:03 -0500

    refactored server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+const routes = require("./controllers/burgers_controller.js");
 
-var app = express();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -11,15 +14,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
-
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
