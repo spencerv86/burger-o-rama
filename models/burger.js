7eff8a43f4ff3fb39179cfb621bb38fe7ec891e4
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-26 20:01:45 -0500

    refactored burger models

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,23 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    all: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
-    },
+  all: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
 
-    create: function (cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-            cb(res);
-        });
-    },
+  create: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
 
-    update: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    }
+  update: function (objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
 };
 
 module.exports = burger;
